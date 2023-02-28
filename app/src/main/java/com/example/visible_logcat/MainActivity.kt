54b103d353cbd617bada474ca2ca954be122646c
snaps-eddy <eddy.jang@snaps.com>
2023-02-28 18:07:09 +0900

    test

--- app/src/main/java/com/example/visible_logcat/MainActivity.kt
+++ app/src/main/java/com/example/visible_logcat/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+//asdflsdaf
         createDebugTool(this) {
             setAutoPermissionCheck(true)
         }.bindService()
